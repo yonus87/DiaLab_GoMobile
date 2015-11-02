5350477c4b663c2753f0620a629e3eb8dc70dc23
kanaka gude <yogi87.cse@gmail.com>
2015-11-02 16:36:10 +0100

    Framework Files
    Django and rest_api files

---
+++ src/DiaLab_GoMobileApi/serializers.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 27, 2015
+
+@author: Yogendra
+'''
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+        
